b2d7027e42a9b645c6985fd9763e44160031471f
YEthYuan <maxwell.yuanyeh@gmail.com>
2024-02-20 13:36:55 -0500

    feat: basic rag setup

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 import argparse
 
+from langchain import hub
+from langchain_community.llms import GPT4All
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
 from rag.doc_reader import QuestionAnsweringModel, ContextSelector, AnswerProcessor
 from rag.embedder import EmbeddingModel, Embedder
 from rag.retriever import DocumentDatabase, Ranker, Retriever
 
 
+
 def main():
+
+    # NOT IMPLEMENTED
+    raise NotImplementedError
+
     # Initialize components
     embedding_model = EmbeddingModel()
     embedder = Embedder(embedding_model)
@@ -25,12 +40,53 @@ def main():
     print(processed_answer)
 
 
+# Temporarily solution
+def langchain(args):
+    loader = DirectoryLoader(args.doc_path)
+    data = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
+
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
+
+    # prepare core model
+    gpt4all = GPT4All(model=args.model_ckpt, max_tokens=args.max_tokens)
+
+    # Prompt
+    rag_prompt = hub.pull("rlm/rag-prompt")
+
+    # Chain
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    retriever = vectorstore.as_retriever()
+    chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | rag_prompt
+            | gpt4all
+            | StrOutputParser()
+    )
+
+    # Run
+    invoke_result = chain.invoke(args.question)
+    print(invoke_result)
+
+
 def arg_parser():
     parser = argparse.ArgumentParser(description="LTI Neural Navigator")
+    parser.add_argument("--question", type=str,
+                        default="What are the approaches to Task Decomposition?",
+                        help="Question to ask")
+    parser.add_argument("--max_tokens", type=int, default=2048, help="Maximum tokens supported by the system")
+
     parser.add_argument("--doc_path", type=str, default="../data/sample", help="Path to knowledge base")
-    parser.add_argument("--model_ckpt", type=str, default="../model_ckpts/????", help="Path to model checkpoint")
+    parser.add_argument("--core_model_ckpt", type=str, default="../model_ckpts/????", help="Path to core model checkpoint")
+    parser.add_argument("--embed_model_ckpt", type=str, default="../model_ckpts/????", help="Path to model checkpoint")
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    args = arg_parser()
+    langchain(args)
