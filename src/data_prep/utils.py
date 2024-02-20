783800874c33e7a70809c7aebc5a6eb681914515
U1ltra <118020229@link.cuhk.edu.cn>
2024-02-20 08:58:54 -0500

    feat: handling pdf links

--- src/data_prep/utils.py
+++ src/data_prep/utils.py
@@ -1,10 +1,12 @@
 import os
 import json
+import urllib.request
+
+from tqdm import tqdm
 from datetime import datetime
 from bs4 import BeautifulSoup
-from collections import defaultdict
 from typing import Union, List
-from tqdm import tqdm
+from collections import defaultdict, Iterator
 from selenium.common.exceptions import TimeoutException, WebDriverException
 
 
@@ -21,9 +23,28 @@ def save_html(soup: BeautifulSoup, path: str, page_title: str = None):
         file.write(str(soup))
 
 
+def save_pdf(url: str, path: str):
+    datetime_str = datetime.now().strftime("%Y-%m-%d")
+    path = f"{path}/{datetime_str}/{url.split('/')[-1].replace('%', '_')}"
+
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+
+    try:
+        urllib.request.urlretrieve(
+            url, path
+        )  # TODO: assuming no pdfs have the same name
+    except Exception as e:  # TODO: HTTP Error 403: forbidden
+        tqdm.write(f"Error: {e}")
+        return
+    return url.split("/")[-1].replace("%", "_")
+
+
 def bfs_pages(
     scraper,
-    url: Union[str, List[str]],
+    url: Union[
+        str, List[str], Iterator[str]
+    ],  # Iterator[str] or Generator[str, None, None] (Generator[YieldType, SendType, ReturnType])
     visited: defaultdict[str, str],
     depth: int = 0,
     max_depth: int = 2,
@@ -37,10 +58,22 @@ def bfs_pages(
         url = [url]
 
     for u in tqdm(url):
-        if u in visited:
-            continue
+        # if u in visited:  # TODO: should skip here or after parsing?
+        #     continue
+
         tqdm.write(f"Visiting {u}")
 
+        # if the page is a pdf download it to the data/raw folder
+        if u.endswith(".pdf"):
+            try:
+                visited[u] = save_pdf(
+                    u, "data/raw"
+                )  # TODO: will pdf contain any useful links?
+            except Exception as e:
+                tqdm.write(f"Error: {e}")
+            continue
+
+        # otherwise parse the page and get the links
         try:
             soup = scraper.parse(u)
         except TimeoutException as e:
@@ -53,8 +86,10 @@ def bfs_pages(
         links.extend(
             scraper.get_links(soup)
         )  # TODO: should I filter out only cmu links?
-        visited[u] = scraper.get_title(soup)
-        save_html(soup, "data/raw", visited[u])
+
+        if u not in visited:  # only save the page if it hasn't been visited before
+            visited[u] = scraper.get_title(soup)
+            save_html(soup, "data/raw", visited[u])
 
     bfs_pages(scraper, links[29:], visited, depth + 1)
 
