a209e92121f1bbe5e73db2deb648c2205ae36a45
YEthYuan <maxwell.yuanyeh@gmail.com>
2024-03-10 23:30:59 -0400

    feat: temp

--- src/main.py
+++ src/main.py
@@ -1,18 +1,52 @@
 import argparse
+import os
+from typing import List
 
+import torch
 from langchain import hub
-from langchain_community.llms import GPT4All
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from transformers import BitsAndBytesConfig
 
 from rag.doc_reader import QuestionAnsweringModel, ContextSelector, AnswerProcessor
 from rag.embedder import EmbeddingModel, Embedder
 from rag.retriever import DocumentDatabase, Ranker, Retriever
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description="LTI Neural Navigator")
+    # parser.add_argument("--question", type=str,
+    #                     default="Where is CMU located?",
+    #                     help="Question to ask")
+    parser.add_argument("--test_set", type=str, default="test_set.txt", help="Path to test set")
+    parser.add_argument("--topk", type=int, default=5, help="Maximum number of documents to retrieve")
+    parser.add_argument("--generate_batch_size", type=int, default=0,
+                        help="Batch size for generation, 0 for no batching")
+    parser.add_argument("--max_tokens", type=int, default=2048, help="Maximum tokens supported by the system")
+    parser.add_argument("--search_type", type=str, default="mmr", help="Type of search to perform",
+                        choices=["similarity", "mmr", "similarity_score_threshold"])
+    parser.add_argument("--device", type=str, default="cuda", help="Device to run the model on")
+
+    parser.add_argument("--knowledge_base", type=str, default="/home/ubuntu/rag-project/data/sample",
+                        help="Path to knowledge base")
+    parser.add_argument("--core_model_id", type=str,
+                        default="meta-llama/Llama-2-7b-hf",
+                        help="Model ID for the core model")
+    parser.add_argument("--embed_model_id", type=str,
+                        default="mixedbread-ai/mxbai-embed-large-v1",
+                        # default="BAAI/bge-small-en",
+                        help="Model ID for the embedding model")
+
+    parser.add_argument("--cache_dir", type=str, default="/mnt/datavol/cache", help="Directory to store cache")
+
+    return parser.parse_args()
 
 
 def main():
@@ -40,70 +74,122 @@ def main():
 
 # Temporarily solution
 def langchain(args):
-    loader = DirectoryLoader(args.doc_path)
+    loader = DirectoryLoader(args.knowledge_base)
     data = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
     all_splits = text_splitter.split_documents(data)
 
+    # prepare embedding model
     embeddings = HuggingFaceEmbeddings(
-        model_name=args.embed_model_ckpt,
-        
+        model_name=args.embed_model_id,
+        model_kwargs={
+            "device": args.device,
+        },
+        encode_kwargs={
+
+        }
     )
 
-    vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
+    # prepare vector store
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
 
     # prepare core model
-    gpt4all = GPT4All(model=args.core_model_ckpt, max_tokens=args.max_tokens)
+    if args.generate_batch_size > 0:
+        gen_pipeline = HuggingFacePipeline.from_pretrained(
+            model_id=args.core_model_id,
+            task="text-generation",
+            device_map="auto",
+            batch_size=args.inference_batch_size,
+            model_kwargs={
+                "temperature": 0.7,
+                "max_length": 4096,
+                "torch_dtype": torch.bfloat16,
+                "load_in_8bit": True,
+                "quantization_config": BitsAndBytesConfig(load_in_8bit=True)
+            }
+        )
+    else:
+        gen_pipeline = HuggingFacePipeline.from_model_id(
+            model_id=args.core_model_id,
+            task="text-generation",
+            tokenizer=
+            device_map="auto",
+            batch_size=1,
+            model_kwargs={
+                "temperature": 0.7,
+                "max_length": 4096,
+                "torch_dtype": torch.bfloat16,
+                "load_in_8bit": True,
+                "quantization_config": BitsAndBytesConfig(load_in_8bit=True)
+            }
+        )
 
-    # Prompt
-    rag_prompt = hub.pull("rlm/rag-prompt")
+    # prepare prompt
+    prompt = hub.pull("yeyuan/rag-prompt-llama")
 
     # Chain
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
 
+    if args.generate_batch_size > 0:
+        # Batch generation mode
+        with open(args.test_set, "r") as f:
+            questions = f.readlines()
+            questions = [q.strip() for q in questions]
+            # get iterator of questions
+            for batch_questions in get_data(questions, args.generate_batch_size):
+                raise NotImplementedError
+                # TODO: https://python.langchain.com/docs/integrations/llms/huggingface_pipelines#batch-gpu-inference
+
+    else:
+        # interactive
+        while True:
+            question = input("What's your problem today (input \"exit\" to exit): ")
+            if question == "exit":
+                break
+
+            # Retrieve documents
+            topk_docs = vectorstore.search(question, k=args.topk, search_type=args.search_type)
+            formatted_topk_docs = format_docs(topk_docs)
+
+            # Rerank
+
+            # Define the chain
+            chain = prompt | gen_pipeline.bind(stop=["\n\n"])
+
+            # Run the chain
+            output = chain.batch([{"question": question, "context": formatted_topk_docs}])
+
+            # Parse the output
+            answer = StrOutputParser().parse(output[0])
+            print("Answer:", answer)
+
+            print("====== Reference documents ======")
+            for i, doc in enumerate(topk_docs):
+                print(f"[{i + 1}] {doc}")
+
+
+def get_data(data: List[str], batch_size: int) -> List[str]:
+    questions_iter = iter(data)
     while True:
-        question = input("What's your problem today (input \"exit\" to exit): ")
-        if question == "exit":
+        batch_questions = []
+        for _ in range(batch_size):
+            try:
+                question = next(questions_iter)
+                batch_questions.append(question)
+            except StopIteration:
+                break
+        if not batch_questions:
             break
-
-        # Retrieve documents
-        topk_docs = vectorstore.search(question, k=args.topk, search_type=args.search_type)
-
-        # Generate prompt
-        prompt = rag_prompt.format(question=question, context=format_docs(topk_docs))
-
-        # Run the model
-        output = gpt4all(prompt)
-
-        # Parse the output
-        answer = StrOutputParser().parse(output)
-        print("Answer:", answer)
-
-        print("====== Reference documents ======")
-        for i, doc in enumerate(topk_docs):
-            print(f"[{i + 1}] {doc}")
-
-
-def arg_parser():
-    parser = argparse.ArgumentParser(description="LTI Neural Navigator")
-    # parser.add_argument("--question", type=str,
-    #                     default="Where is CMU located?",
-    #                     help="Question to ask")
-    parser.add_argument("--topk", type=int, default=5, help="Maximum number of documents to retrieve")
-    parser.add_argument("--max_tokens", type=int, default=2048, help="Maximum tokens supported by the system")
-    parser.add_argument("--search_type", type=str, default="mmr", help="Type of search to perform",
-                        choices=["similarity", "mmr", "similarity_score_threshold"])
-
-    parser.add_argument("--doc_path", type=str, default="../data/sample", help="Path to knowledge base")
-    parser.add_argument("--core_model_ckpt", type=str, default="../model_ckpts/gpt4all-falcon-newbpe-q4_0.gguf",
-                        help="Path to core model checkpoint")
-    parser.add_argument("--embed_model_ckpt", type=str, default="../model_ckpts/????", help="Path to model checkpoint")
-
-    return parser.parse_args()
+        yield batch_questions
 
 
 if __name__ == "__main__":
     args = arg_parser()
+
+    # Do some initializations
+    os.environ['HF_HOME'] = args.cache_dir
+    os.environ['NLTK_DATA'] = args.cache_dir
+
     langchain(args)
